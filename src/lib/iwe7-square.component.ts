d406590ad5fdbf15b269ef3b7c5792eddac2e0ee
iwe7 <admin@meepo.com.cn>
2018-06-05 13:11:55 +0800

    save

--- src/lib/iwe7-square.component.ts
+++ src/lib/iwe7-square.component.ts
@@ -1,8 +1,10 @@
+import { Iwe7IcssService } from 'iwe7-icss';
 import {
   Component,
   OnInit,
   ViewEncapsulation,
   Input,
+  ElementRef,
   Injector
 } from "@angular/core";
 import { BaseWithIcss } from "iwe7-base";
@@ -19,7 +21,7 @@ import { BaseWithIcss } from "iwe7-base";
 export class Iwe7SquareComponent extends BaseWithIcss {
   @Input() width: string;
   @Input() backgroundColor: string;
-  constructor(injector: Injector) {
+  constructor(injector: Injector, public icss: Iwe7IcssService, public ele: ElementRef) {
     super(injector);
     this.setStyleInputs(["width", "backgroundColor"]);
   }
